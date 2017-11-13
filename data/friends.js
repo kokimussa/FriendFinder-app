fb919efb7ba9b6c715bc99d6c0616eeae11d7547
kokimussa <koki.mussa@gmail.com>
2017-11-13 18:24:20 -0500

    homework

---
+++ data/friends.js
@@ -0,0 +1,22 @@
+var friends = [ {
+  "name" : "Jen",
+  "photo" : "images/jen.jpg",
+  "scores" : [5,5,2,3,1,5,2,1,3,5]
+}, 
+
+{
+  "name" : "Stacey",
+   "photo" : "images/Stacey.jpg",
+  "scores" : [1,5,1,1,1,1,1,1,5,5]
+}, 
+
+{
+  "name" : "Joe",
+  "photo" : "images/Joe.jpg",
+  "scores" : [5,3,2,3,2,5,1,3,3,4]
+}
+
+];
+
+
+module.exports = friends;
