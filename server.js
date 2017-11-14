c9b41f27d539dcc0395ee9aefd1c5c87fd67c708
kokimussa <koki.mussa@gmail.com>
2017-11-14 17:57:27 -0500

    fix routes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./routing/apiRoutes.js')(app); 
+require('./routing/htmlRoutes.js')(app);
 
 
 app.listen(PORT, function () {
